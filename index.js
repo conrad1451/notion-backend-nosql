8f6d3ad1226556e61e5df1f98a64a5ecd13ce962
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-29 18:45:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-// Importing express 
-const express = require('express'); 
+// Note: because this is a SERVER, after making changes to this file,
+//       one must RESTART the task to see the changes
 
-// Creating instance of express 
-const app = express(); 
+// Optimization opportunity: refactor code base to query the database
+// only once and read through the local data each time a new user
+// is added
 
-const PORT = process.env.PORT || 8081;
+// const DATA_FROM_FORM = "";
+const BASE_URL = NOTION_API_DATABASE;
 
-// Handling GET / Request 
-app.get('/', function (req, res) { 
-	res.send("Hello World!, I am server created by expresss"); 
-}) 
+const express = require("express");
 
-// Testing custom path
-app.get('/bro/', function (req, res) { 
-	res.send("Ayo!, I'm a new path in this ExpressJS app. What's good?"); 
-}) 
+// Following line is where NodeJS server is coupled to the HTML
+// const moduleToFetch = require(DATA_FROM_FORM);
+// const getDatabase = moduleToFetch.getDatabase;
+// const newEntryToDatabase = moduleToFetch.newEntryToDatabase;
+ 
+const PORT = process.env.PORT || 8000;
 
-// Listening to server at port 3000 
+const app = express();
+
+// app.use(express.static("public"));
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   }),
+// );
+
+// CHQ: Reads all data entries from the database (Read)
+app.get(BASE_URL, async (req, res) => {
+  // res.send("You are currently on the users page. You should be reading data entries from the Notion database"); 
+
+	const users = JSON.parse(req);
+	res.send(req);
+	  // res.json(req);
+  // const users = await getDatabase();
+  // res.json(users);
+});
+
+// // CHQ: Posts a new entry to the database (Create)
+// app.post("/submit-form", async (req, res) => {
+//   const name = req.body.name;
+//   const role = req.body.role;
+//   await newEntryToDatabase(name, role);
+//   res.redirect("/");
+//   res.end();
+// });
+
+// Listening to server at port
 app.listen(PORT, function () { 
 	console.log("server started"); 
 })
