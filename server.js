a785948e55b7fccac941008d0e228dfa9897941b
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-29 18:02:25 -0400

    Test edits to server.js
    Making test edits for its ability to read data from an API. Will add ability to write data to an API.

--- server.js
+++ server.js
@@ -4,38 +4,45 @@
 // Optimization opportunity: refactor code base to query the database
 // only once and read through the local data each time a new user
 // is added
+
+const DATA_FROM_FORM = "";
+
+
 const express = require("express");
 
 // Following line is where NodeJS server is coupled to the HTML
-const moduleToFetch = require("./index");
-const getDatabase = moduleToFetch.getDatabase;
-const newEntryToDatabase = moduleToFetch.newEntryToDatabase;
-const port = 8000;
+// const moduleToFetch = require(DATA_FROM_FORM);
+// const getDatabase = moduleToFetch.getDatabase;
+// const newEntryToDatabase = moduleToFetch.newEntryToDatabase;
+ 
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(express.static("public"));
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-);
+// app.use(express.static("public"));
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   }),
+// );
 
 // CHQ: Reads all data entries from the database (Read)
 app.get("/users", async (req, res) => {
-  const users = await getDatabase();
-  res.json(users);
+  res.send("You are currently on the users page. You should be reading data entries from the Notion database"); 
+  // const users = await getDatabase();
+  // res.json(users);
 });
 
-// CHQ: Posts a new entry to the database (Create)
-app.post("/submit-form", async (req, res) => {
-  const name = req.body.name;
-  const role = req.body.role;
-  await newEntryToDatabase(name, role);
-  // the changes here worked
-  // await newEntryToDatabase("ddd", "dsafdsaf");
-  res.redirect("/");
-  res.end();
-});
+// // CHQ: Posts a new entry to the database (Create)
+// app.post("/submit-form", async (req, res) => {
+//   const name = req.body.name;
+//   const role = req.body.role;
+//   await newEntryToDatabase(name, role);
+//   res.redirect("/");
+//   res.end();
+// });
 
-app.listen(port, console.log(`Server started on ${port}`));
+// Listening to server at port
+app.listen(PORT, function () { 
+	console.log("server started"); 
+})
